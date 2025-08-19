177cdadb7437e0334cf04fea3ef0dec314c75715
samyogadk24 <samyogdbs19@gmail.com>
2025-08-19 08:41:43 +0545

    program

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
+
+
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Worid!");
+        System.out.println("Hello, World!");
     }
 }                 
  
-  
