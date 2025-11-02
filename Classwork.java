f6db6eda855e8f784712a869ec2f2cf8929c8ade
samyogadk24 <samyogdbs19@gmail.com>
2025-11-02 14:00:25 -0800

    Added positivenegative

---
+++ Classwork.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Classwork {
+
+    public static void main (String[]args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println ("enter a number");
+        int number = input.nextInt();
+
+        if (number > 0 ){
+            System.out.println (" The number is Positive");
+        } else if (number < 0) {
+            System.out.println (" The number is Negative");
+        }
+              input.close();
+        }
+        }
+    
---
+++ day_1/Operators.java
@@ -0,0 +1,12 @@
+public class Operators {
+
+    public static void main(string args []){
+        int a = 10;
+        int b =5;
+
+    system.out.println(a+b);
+        
+
+    } 
+    
+} 
