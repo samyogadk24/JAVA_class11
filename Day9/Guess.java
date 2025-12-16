eee816323fb498420bb65d45ff1fc3b7e7f32b31
samyogadk24 <samyogdbs19@gmail.com>
2025-12-16 01:50:35 -0800

    cw

---
+++ Day9/Guess.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Guess {
+
+
+    int number;
+    int count=0;
+
+
+    public Guess(int number) {
+        this.number = number;
+    }
+
+    void guessNum() {
+
+        Scanner input = new Scanner(System.in);
+        boolean correct = true;
+
+        while (correct) {
+            System.out.print("Enter the number : ");
+            int num = input.nextInt();
+            count ++;
+
+
+            if (num < number) {
+                System.out.println("Your number is less than my guess");
+            } 
+            else if (num > number) {
+                System.out.println("Your number is greater than my guess");
+            } 
+            else {
+                System.out.println("Your number is equal to my guessing number");
+                correct = false;
+            }
+        }
+        System.out.println("You have guess the number in " +count+" attemps");
+
+    }
+
+    public static void main(String[] args) {
+
+        Guess guess = new Guess(25);
+        guess.guessNum();
+    }
+}
